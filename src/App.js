0bda0b2387ac43fa107f20b85e77d31d333e1a02
Msaeed144 <mohammadsaeed76@gmail.com>
2023-08-05 18:49:55 +0330

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { Switch , Redirect ,Route } from 'react-router-dom/cjs/react-router-dom.min';
 //Components
 import Store from './components/Store';
 //Context
 import ProductContextProvider from './context/ProductContextProvider';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
     <ProductContextProvider>
-      <Store />
+      <Switch>
+          <Route path='/products/:id'component={ProductDetails} />
+          <Route path='/products' component={Store} />
+          <Redirect to='products' />
+      </Switch>
     </ProductContextProvider>
   );
 }
