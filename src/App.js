d19b27bb15f8aef1599042df88e0d63b1fa91bd4
Msaeed144 <mohammadsaeed76@gmail.com>
2023-08-05 16:44:41 +0330

    add store to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
+//Components
+import Store from './components/Store';
 //Context
 import ProductContextProvider from './context/ProductContextProvider';
 
 function App() {
   return (
     <ProductContextProvider>
-      
+      <Store />
     </ProductContextProvider>
   );
 }
