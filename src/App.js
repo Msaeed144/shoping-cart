9be1bbba1db3f86549fd9ced71b9eb65177d593e
Msaeed144 <mohammadsaeed76@gmail.com>
2023-08-08 12:08:41 +0330

    add CART CONTEXT to app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import { Switch , Redirect ,Route } from 'react-router-dom/cjs/react-router-dom.min';
 //Components
 import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
+
 //Context
 import ProductContextProvider from './context/ProductContextProvider';
-import ProductDetails from './components/ProductDetails';
+import ProductContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
     <ProductContextProvider>
-      <Switch>
-          <Route path='/products/:id'component={ProductDetails} />
-          <Route path='/products' component={Store} />
-          <Redirect to='products' />
-      </Switch>
+      <CartContextProvider>
+        <Switch>
+            <Route path='/products/:id'component={ProductDetails} />
+            <Route path='/products' component={Store} />
+            <Redirect to='products' />
+        </Switch>
+      </CartContextProvider>
     </ProductContextProvider>
   );
 }
