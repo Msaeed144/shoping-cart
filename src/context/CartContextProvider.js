054f0a8c6e011dc81be886f63550a54ba798d60d
Msaeed144 <mohammadsaeed76@gmail.com>
2023-08-08 11:55:12 +0330

    create use Reduser and create two actions:ADDITENS and REMOVEITEMS

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -1,6 +1,38 @@
 import React from 'react';
+import { useReducer , useContext } from 'react';
 
+const initialstate = {
+    selectedItems : [],
+    itemsCounter: 0,
+    total: 0,
+    checkout: false
+}
+const cartReducer = (state , action) => {
+    switch(action.type){
+        case "ADD_ITEM":
+            if(!state.selectedItems.find(item => item.id === action.payload.id)) {
+                state.selectedItems.puch({
+                    ...action.payload,
+                    quantity: 1
+                })
+            }
+            return {
+                ...state,
+                selectedItems: [...state.selectedItems]
+            }
+        case "REMOVE_ITEM":
+            const newSelectedItems = state.selectedItems.filter(item =>item.id !== action.payload.id);
+            return {
+                ...state,
+                selectedItems:[...newSelectedItems]
+            }
+
+    }
+}
 const CartContextProvider = () => {
+
+    const [ state , dispatch ] = useReducer(cartReducer , initialstate)
+
     return (
         <div>
             
